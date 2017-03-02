e214e3f4fba1c358fca22ad183be6572f0767aa7
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2017-03-02 12:22:44 +0530

    Re-add support for history *per session* in REPL

--- src/repl.rs
+++ src/repl.rs
@@ -15,6 +15,7 @@ pub fn run_repl() {
         let readline = rl.readline("> ");
         match readline {
             Ok(line) => {
+                rl.add_history_entry(&line);
                 let orig_input = String::from(line.trim());
                 let mut input = orig_input.clone();
                 if !input.ends_with(";") {
