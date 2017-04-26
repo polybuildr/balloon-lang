518da87025d69a0320a39bf00c8edb50df661edd
Vivek Ghaisas <polybuildr@users.noreply.github.com>
2017-04-26 23:31:58 +0530

    Run lint.sh on Travis (#18)

--- lint.sh
+++ lint.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# Run cargo check and cargo clippy to lint code
+# Run cargo check and cargo clippy (if nightly) to lint code
 # Running clippy also runs `cargo check`
 # The `-D clippy` flag forces all warnings to be errors
-echo "Running cargo check and clippy..."
-cargo clippy -- -D clippy
-CLIPPY_EXIT_CODE=$?
+if rustc --version | grep "nightly"
+then
+    echo "On nightly."
+    echo "Running cargo clippy..."
+    cargo rustc --features "clippy cargo-clippy" -- -Z no-trans -Z extra-plugins=clippy
+    CARGO_EXIT_CODE=$?
+else
+    echo "Not on nightly."
+    echo "Running only cargo check..."
+    cargo check
+    CARGO_EXIT_CODE=$?
+fi
 
 # Run rustfmt in diff mode, exits with error status if
 # there is any diff
@@ -14,5 +23,6 @@ cargo fmt -- --write-mode diff
 RUSTFMT_EXIT_CODE=$?
 
 # Exit if either of the exit codes was non-zero
-if [[ $CLIPPY_EXIT_CODE -ne 0 ]]; then exit 1; fi
-if [[ $RUSTFMT_EXIT_CODE -ne 0 ]]; then exit 1; fi
+if [[ $CARGO_EXIT_CODE -ne 0 ]]; then echo "Error!"; exit 1; fi
+if [[ $RUSTFMT_EXIT_CODE -ne 0 ]]; then echo "Error!"; exit 1; fi
+echo "Done."
