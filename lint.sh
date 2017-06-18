1323e28672bb7836192272c482527cddd2e4fd55
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2017-06-18 17:57:09 +0300

    Update .travis.yml and lint.sh to account for rustfmt-nightly

--- lint.sh
+++ lint.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Run cargo check and cargo clippy (if nightly) to lint code
-# Running clippy also runs `cargo check`
-# The `-D clippy` flag forces all warnings to be errors
+# Run cargo check (if not nightly), cargo clippy (if nightly) and
+# cargo fmt (if nightly) to lint code
 if rustc --version | grep "nightly"
 then
     echo "On nightly."
     echo "Running cargo clippy..."
     cargo rustc --features "clippy cargo-clippy llvm-backend" -- -Z no-trans -Z extra-plugins=clippy
     CARGO_EXIT_CODE=$?
+    # Run rustfmt-nightly in diff mode, exits with error status if
+    # there is any diff
+    echo "Running rustfmt..."
+    cargo fmt -- --write-mode diff
+    RUSTFMT_EXIT_CODE=$?
 else
     echo "Not on nightly."
     echo "Running only cargo check..."
     cargo check --features "llvm-backend"
     CARGO_EXIT_CODE=$?
+    # Didn't run rustfmt, so claim there were no errors
+    RUSTFMT_EXIT_CODE=0
 fi
-
-# Run rustfmt in diff mode, exits with error status if
-# there is any diff
-echo "Running rustfmt..."
-cargo fmt -- --write-mode diff
-RUSTFMT_EXIT_CODE=$?
 
 # Exit if either of the exit codes was non-zero
 if [[ $CARGO_EXIT_CODE -ne 0 ]]; then echo "Error!"; exit 1; fi
